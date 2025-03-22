3fb91ea3eb0e6a2d9b1b099848c1f79de0d8837b
Sumit De <sumitde24012004@gmail.com>
2025-03-22 19:57:29 +0530

    simple express server create

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// root file (server created)
+const express = require("express");
+require("dotenv").config();
+let cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+//server listen
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  try {
+    console.log("App is running on 3001");
+  } catch (error) {
+    console.log("Error", error.message);
+  }
+});
