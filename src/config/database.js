cb6f2b56d7a073c0bd5ecd134d6126afe82f7a0c
Sumit De <sumitde24012004@gmail.com>
2025-03-24 15:04:16 +0530

    environment setup

--- src/config/database.js
+++ src/config/database.js
@@ -1,1 +1,24 @@
-// a/c to the environment db changes
+require('dotenv').config({
+    path: `.env.${process.env.NODE_ENV || 'development'}`
+})
+
+module.exports={
+    development:{
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST
+    },
+    staging:{
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST
+    },
+    production:{
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST
+    }
+}
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,15 +1,10 @@
 const { Sequelize } = require("sequelize");
-require("dotenv").config();
+const config = require('./database')[process.env.NODE_ENV || 'development']
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize( config.database, config.username, config.password,{
+    host: config.host,
     dialect: "mysql",
     //   logging: false
-  }
-);
+});
 
 module.exports = sequelize;
