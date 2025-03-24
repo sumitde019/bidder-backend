cb3fc2211830a90e7474725059fbdb15fbb00a72
Sumit De <sumitde24012004@gmail.com>
2025-03-24 20:49:07 +0530

    model updation

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 require("dotenv").config();
 let cors = require("cors");
 const sequelize = require("./src/config/dbConnect");
+const Roles = require("./src/models/role");
+const Users = require("./src/models/user");
 const app = express();
 
 app.use(express.json());
@@ -15,6 +17,8 @@ app.listen(PORT, async () => {
     //db connection
     await sequelize.authenticate();
     console.log("Db Connection has been established successfully.");
+    await Roles.sync({force:false});
+    await Users.sync({force:false});
   } catch (error) {
     console.log("Error", error.message);
   }
