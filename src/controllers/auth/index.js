1d505156973f9ac59e5fa546cff4b285a758d73f
Sumit De <sumitde24012004@gmail.com>
2025-03-26 00:58:15 +0530

    index updated

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -1,15 +1,26 @@
-const authService = require('../../service/auth/authService');
-const { sendSuccessResponse } = require('../../utils/response');
+const authService = require("../../service/auth/authService");
+const {
+  SUCCESS_MESSAGE,
+  ERROR_MESSAGE,
+} = require("../../utils/propertyresolver");
+const {
+  sendSuccessResponse,
+  sendErrorResponse,
+} = require("../../utils/response");
 
-const registerUser=async (req,res)=>{
-    try {
-        const userInfo = await authService.saveUser(req.body);
-        // res.send(userInfo);
-        sendSuccessResponse(res,)
-    } catch (error) {
-        res.send(error.message);
-    }
+const registerUser = async (req, res) => {
+  try {
+    const userInfo = await authService.saveUser(req.body);
+    
+    sendSuccessResponse(res, SUCCESS_MESSAGE.USER_CREATED, userInfo, 200);
+  } catch (error) {
+    sendErrorResponse(
+      res,
+      error.message || ERROR_MESSAGE.SOMETHING_WENT_WRONG,
+      "",
+      500
+    );
+  }
 };
 
-
-module.exports = {registerUser};
+module.exports = { registerUser };
