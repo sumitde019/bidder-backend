fcd587122072dfc2f5058aba455a8976c16bb586
Sumit De <sumitde24012004@gmail.com>
2025-03-27 11:14:36 +0530

    verification-email added

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -8,15 +8,22 @@ const {
   sendErrorResponse,
 } = require("../../utils/response");
 const bcrypt = require("bcrypt");
+const {v4:uuidv4} = require('uuid');
 
 const registerUser = async (req, res) => {
   try {
     const { password } = req.body;
     const hashPassword = await bcrypt.hash(password, 10);
 
+    // Generate token
+    const accountToken = uuidv4();
+    const accountTokenExpiry = Date.now() + 3600000; //token valid for 1 hr
+
     const userInfo = await authService.saveUser({
       ...req.body,
       password: hashPassword,
+      verify_account_token: accountToken,
+      verify_account_expires: accountTokenExpiry 
     });
 
     sendSuccessResponse(res, SUCCESS_MESSAGE.USER_CREATED, userInfo, 200);
---
+++ src/service/mail/mailService.js
@@ -0,0 +1,34 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV || "development"}`,
+});
+
+const sendMail = async (userEmail, mailSubject, content) => {
+  //1. create an email transporter
+  // SMTP (Simple Mail Transfer Protocol)
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "user069mail@gmail.com",
+      pass: process.env.EMAIL_PASSWORD, 
+    },
+  });
+
+  //2. Config email content
+  const mailOption = {
+    from: "user069mail@gmail.com",
+    to: userEmail,
+    subject: mailSubject,
+    html: content,
+  };
+
+  //3. Send Mail
+  try {
+    const result = await transporter.sendMail(mailOption);
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+module.exports = sendMail;
