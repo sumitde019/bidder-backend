b67d316d9cf349a426c4bc721ea256f0592fb2fc
Sumit De <sumitde24012004@gmail.com>
2025-03-28 11:57:43 +0530

    forgot password API

---
+++ src/middlewares/validationSchema/forgotPassword.js
@@ -0,0 +1,6 @@
+const Joi = require("joi");
+
+const forgotPasswordSchema = Joi.object({
+  email: Joi.string().email().required(),
+});
+module.exports = forgotPasswordSchema;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,11 +3,13 @@ const authController = require('../controllers/auth/index')
 const validateSchema = require("../middlewares/validator");
 const registerSchema = require('../middlewares/validationSchema/registerUser');
 const loginSchema = require('../middlewares/validationSchema/loginUser');
+const forgotPasswordSchema = require('../middlewares/validationSchema/forgotPassword');
 const authRouter = express.Router();
 
 authRouter.post('/register',validateSchema(registerSchema), authController.registerUser);
 authRouter.get("/verify-account/:token",authController.verifyAccount);
 authRouter.post("/login",validateSchema(loginSchema), authController.loginUser);
+authRouter.post("/forgot-password",validateSchema(forgotPasswordSchema), authController.forgotPassword);
 
 module.exports = authRouter;
 
